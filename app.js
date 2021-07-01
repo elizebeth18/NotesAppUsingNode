02771d596cf7b36fd18dbf5395ef22a355d4388b
Jilu <elizebeth18.jilu@gmail.com>
2021-07-01 19:31:11 +0530

    usage of npm packages

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-
+const validator = require('validator');
+const chalk = require('chalk');
 const getNotes = require('./notes');
 
 
 const fetchNotes = getNotes();
 console.log(fetchNotes);
+
+console.log("Valid e-mail :"+validator.isEmail('elizebeth18.jilu@example.com'));
+console.log("Valid e-mail :"+validator.isEmail('example.com'));
+console.log("Valid URL :"+validator.isURL('https://mead.io'));
+console.log("Valid URL :"+validator.isURL('htclea/mead.io'));
+
+console.log(chalk.green('Success'));
+
