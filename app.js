e3ccbce217b698c906bcf3b187278557778cf980
Jilu <elizebeth18.jilu@gmail.com>
2021-07-03 03:29:45 +0530

    checking after removal of node modules

--- app.js
+++ app.js
@@ -6,10 +6,7 @@ const getNotes = require('./notes');
 const fetchNotes = getNotes();
 console.log(fetchNotes);
 
-console.log("Valid e-mail :"+validator.isEmail('elizebeth18.jilu@example.com'));
-console.log("Valid e-mail :"+validator.isEmail('example.com'));
-console.log("Valid URL :"+validator.isURL('https://mead.io'));
-console.log("Valid URL :"+validator.isURL('htc/mead.io'));
+
 
 console.log(chalk.blue.inverse.bold('Success'));
 console.log(chalk.hex('#DEADED').underline.inverse('Hello, world!'))
