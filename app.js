c0eb9b509092200356f2b9f91622963a61431a9f
Jilu <elizebeth18.jilu@gmail.com>
2021-07-13 21:59:47 +0530

    used try-ctach block for adding notes

--- app.js
+++ app.js
@@ -1,13 +1,59 @@
 // const validator = require('validator');
 const chalk = require('chalk');
-const getNotes = require('./notes');
+const yargs = require('yargs')
+const notes = require('./notes');
 
+//yargs version
+yargs.version('1.1.0');
 
-const fetchNotes = getNotes();
-console.log(fetchNotes);
+//create "add" command using yargs
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    //to pass arguments to command
+    //the "builder" object is required
+    builder:{
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv){
+        notes.addNote(argv.title, argv.body);
+    }
+})
 
+//create "remove" command using yargs
+yargs.command({
+    command: 'remove',
+    describe: 'To delete a note',
+    handler: function(){
+        console.log('To delete a note');
+    }
+})
 
+//create "list" command using yargs
+yargs.command({
+    command: 'list',
+    describe: 'To list notes',
+    handler: function(){
+        console.log('To list notes');
+    }
+})
 
-console.log(chalk.blue.inverse.bold('Success'));
-console.log(chalk.hex('#DEADED').underline.inverse('Hello, world!'))
+//create "read" command using yargs
+yargs.command({
+    command: 'read',
+    describe: 'To read the notes',
+    handler: function(){
+        console.log('To read the notes');
+    }
+})
 
+console.log(yargs.parse());
--- notes.js
+++ notes.js
@@ -1,6 +1,36 @@
-const getNotes = function (){
+const fs = require('fs');
+
+const getNotes = function () {
     return "Your notes...";
 }
 
+const addNote = function (title, body) {
+    const notes = loadNotes();
 
-module.exports = getNotes;
+    notes.push({
+        title :title,
+        body : body 
+    });
+
+    saveNotes(notes);
+}
+
+const saveNotes = function(notes){
+    const dataJSON = JSON.stringify(notes);
+    fs.writeFileSync('notes.json',dataJSON);
+}
+
+const loadNotes = function () {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json');
+        const dataJSON = dataBuffer.toString();
+        return JSON.parse(dataJSON);
+    } catch (e) {
+        return [];
+    }
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote
+};
